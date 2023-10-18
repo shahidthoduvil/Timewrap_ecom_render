5fd41dbeaadd3b9abd0393c00c84b6d0269a88eb
Shahid <shahidthoduvil.07@gmail.com>
2023-10-18 13:31:38 +0530

    updating the Setting.py1

--- ecom_Timewrap/settings.py
+++ ecom_Timewrap/settings.py
@@ -142,11 +142,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
 
 MEDIA_URL='/MEDIA/'
+
+
 
 MEDIA_ROOT=BASE_DIR/ 'uploads'
 
